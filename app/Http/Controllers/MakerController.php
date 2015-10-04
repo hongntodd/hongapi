be3b8fdda828da30f282463a410bec65e0cefe27
Hong Todd <jjhomeus@yahoo.com>
2015-10-04 12:05:20 -0500

    Implementing Delete method

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Maker;
+use App\Vehicle;
 use App\Http\Requests\CreateMakerRequest;
 
 class MakerController extends Controller {
@@ -85,7 +86,20 @@ class MakerController extends Controller {
 	 */
 	public function destroy($id)
 	{
-		//
+		$maker = Maker::find($id);
+		if(!$maker)
+		{
+			return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
+		}
+
+		$vehicles = $maker->vehicles;
+		if(sizeof($vehicles) > 0)
+		{
+			return response()->json(['message' => 'This maker have associated vehicles. Delete his vehicles first ', 'code' => 409], 409);
+		}
+
+		$maker->delete();
+		return response()->json(['message' => 'This maker has been deleted'], 200);
 	}
 
 }
