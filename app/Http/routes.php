79c6652897694e83a51c5b1655de4f8485582b5f
Hong Todd <jjhomeus@yahoo.com>
2015-10-11 22:37:14 -0500

    Add version prefix for routes.php
    Route::group(array('prefix' => 'api/v1.1'), function(){
    Route::resource('makers', 'MakerController', ['except' => ['create',
    'edit']]);
    
    Route::resource('vehicles', 'VehicleController', ['only' => ['index']]);
    
    Route::resource('makers.vehicles', 'MakerVehiclesController',  ['except'
    => ['create', 'edit']]);
    });

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,8 +12,10 @@
 */
 
 //Route::get('/{name?}', 'MyController@index');
-Route::resource('makers', 'MakerController', ['except' => ['create', 'edit']]);
+Route::group(array('prefix' => 'api/v1.1'), function(){
+	Route::resource('makers', 'MakerController', ['except' => ['create', 'edit']]);
 
-Route::resource('vehicles', 'VehicleController', ['only' => ['index']]);
+	Route::resource('vehicles', 'VehicleController', ['only' => ['index']]);
 
-Route::resource('makers.vehicles', 'MakerVehiclesController',  ['except' => ['create', 'edit']]);
+	Route::resource('makers.vehicles', 'MakerVehiclesController',  ['except' => ['create', 'edit']]);	
+});
